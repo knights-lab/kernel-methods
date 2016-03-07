d5c05b3da70c5c04428c7c5d0416afc564e4fbd6
Nicholas E Sohre <sohre@teraminx.cs.umn.edu>
2016-03-07 13:04:47 -0600

    adding files

--- examples/gevers_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-Rscript src/runKerns.r 'data/mapping/Gevers_CCFA_RISK_study_1939_mapping_file.txt' 'data/Gevers_Taxa/Gevers_CCFA_RISK_study_1939_gg_ref_13_8_L7.txt' 'data/unifrac/weighted_unifrac_Gevers_CCFA_RISK_study_1939_gg_ref_13_8.txt' "" 'ANTIBIOTICS' "c('no')" "c('None','yes')" "Gevers_Antibiotics"
